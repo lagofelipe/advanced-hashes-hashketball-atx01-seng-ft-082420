4fe208007bcea995f301f155066c348a589f4e0c
Felipe Dolago <lagofelipe@gmail.com>
2020-08-09 17:15:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -122,30 +122,30 @@ end
 
 def num_points_scored(player_name)
 
-# SOLUTION 1 - naive
-# some_hash.each do |key, value|
-# game_hash.each do |team, team_data|
-#   team_data.each do |attr, data|
-#     if attr == :players
-#       found_player = data.find do |player|
-#         player[:player_name] == player_name
-#       end
-#
-#       if found_player
-#         return found_player[:points]
-#       end
-#     end
-#   end
-# end
+SOLUTION 1 - naive
+ some_hash.each do |key, value|
+game_hash.each do |team, team_data|
+  team_data.each do |attr, data|
+  if attr == :players
+      found_player = data.find do |player|
+        player[:player_name] == player_name
+      end
+
+      if found_player
+        return found_player[:points]
+      end
+    end
+ end
+ end
 
 # SOLUTION 2 - good
-# game_hash.each do |team, team_data|
-#   team_data[:players].each do |player|
-#     if player[:player_name] == player_name
-#       return player[:points]
-#     end
-#   end
-# end
+game_hash.each do |team, team_data|
+ team_data[:players].each do |player|
+ if player[:player_name] == player_name
+     return player[:points]
+   end
+  end
+ end
 
 # SOLUTION 3
 # get a list of all the players
