0922afb4cdc22a1d2157a4899d7faadd0ff3f2ee
Felipe Dolago <lagofelipe@gmail.com>
2020-08-09 17:04:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,73 +1,9 @@
-# Write your code below game_hash
 def game_hash
   {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
+    away: { team_name: 'Charlotte Hornets',
+      colors: %w[Turquoise Purple],
       players: [
-        {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
-        },
-        {
-          player_name: "Reggie Evans",
-          number: 30,
-          shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
-        },
-        {
-          player_name: "Brook Lopez",
-          number: 11,
-          shoe: 17,
-          points: 17,
-          rebounds: 19,
-          assists: 10,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 15
-        },
-        {
-          player_name: "Mason Plumlee",
-          number: 1,
-          shoe: 19,
-          points: 26,
-          rebounds: 11,
-          assists: 6,
-          steals: 3,
-          blocks: 8,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Jason Terry",
-          number: 31,
-          shoe: 15,
-          points: 19,
-          rebounds: 2,
-          assists: 2,
-          steals: 4,
-          blocks: 11,
-          slam_dunks: 1
-        }
-      ]
-    },
-    away: {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
+        { player_name: 'Jeff Adrien',
           number: 4,
           shoe: 18,
           points: 10,
@@ -75,10 +11,8 @@ def game_hash
           assists: 1,
           steals: 2,
           blocks: 7,
-          slam_dunks: 2
-        },
-        {
-          player_name: "Bismack Biyombo",
+          slam_dunks: 2 },
+        { player_name: 'Bismack Biyombo',
           number: 0,
           shoe: 16,
           points: 12,
@@ -86,10 +20,8 @@ def game_hash
           assists: 7,
           steals: 22,
           blocks: 15,
-          slam_dunks: 10
-        },
-        {
-          player_name: "DeSagna Diop",
+          slam_dunks: 10 },
+        { player_name: 'DeSagna Diop',
           number: 2,
           shoe: 14,
           points: 24,
@@ -97,10 +29,8 @@ def game_hash
           assists: 12,
           steals: 4,
           blocks: 5,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Ben Gordon",
+          slam_dunks: 5 },
+        { player_name: 'Ben Gordon',
           number: 8,
           shoe: 15,
           points: 33,
@@ -108,10 +38,8 @@ def game_hash
           assists: 2,
           steals: 1,
           blocks: 1,
-          slam_dunks: 0
-        },
-        {
-          player_name: "Kemba Walker",
+          slam_dunks: 0 },
+        { player_name: 'Kemba Walker',
           number: 33,
           shoe: 15,
           points: 6,
@@ -119,11 +47,56 @@ def game_hash
           assists: 12,
           steals: 7,
           blocks: 5,
-          slam_dunks: 12
-        }
-      ]
-    }
+          slam_dunks: 12 }
+            ] },
+    home: { team_name: 'Brooklyn Nets',
+      colors: %w[Black White],
+      players: [
+        { player_name: 'Alan Anderson',
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1 },
+        { player_name: 'Reggie Evans',
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7 },
+        { player_name: 'Brook Lopez',
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15 },
+        { player_name: 'Mason Plumlee',
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5 },
+        { player_name: 'Jason Terry',
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1 }
+      ] }
   }
 end
-
-# Write code here
