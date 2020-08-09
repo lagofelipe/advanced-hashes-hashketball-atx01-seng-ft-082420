b700e5d0d1b4ac377231dc87c6ae33bab2774d8d
Felipe Dolago <lagofelipe@gmail.com>
2020-08-09 17:05:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -122,7 +122,7 @@ def shoe_size(name)
 end
 
 def team_colors(team_input)
-  if team_input.downcase == "charlotte hornets" 
+  if team_input.downcase == "charlotte hornets"
     return game_hash[:away][:colors]
   else return game_hash[:home][:colors]
   end
@@ -137,7 +137,7 @@ end
 def player_numbers(input)
   output = []
   game_hash.each do |team, team_info|
-    if team_info[:team_name] == input 
+    if team_info[:team_name] == input
       team_info.each do |key, value|
         if key == :players
           value.each do |player|
